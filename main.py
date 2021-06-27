f9b152c1d5ad0311aee9afa64018d8ad133385e6
nisrine <34342007+NisrineJednane@users.noreply.github.com>
2021-06-27 13:36:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-user_choice = (input("Type 'encode' to encrypt text. Type 'decode' to decrypt text.\n")).lower()
+user_choice = input("Type 'encode' to encrypt or 'decode' to decrypt:\n").lower()
 user_text = input("Type your text:\n")
 user_shift = int(input("Type your shift:\n"))
 
-
-def encode(text, shift):
+def caesar(text, shift):
   result = ""
-  for letter in text:
-    letter_position = alphabet.index(letter)
-    coded_position = letter_position + shift
-    new_letter = alphabet[coded_position]
-    result+=new_letter
-  print(f"The encrypted text is {result}")
+  for i in text:
+    position = alphabet.index(i)
+    if user_choice == "encode":
+      i = alphabet[position+shift]
+      result+=i
+    if user_choice == "decode":
+      i = alphabet[position-shift]
+      result += i
+  print(result)
 
 
-def decode(text, shift):
-  result = ""
-  for letter in text:
-    letter_position = alphabet.index(letter)
-    coded_position = letter_position - shift
-    new_letter = alphabet[coded_position]
-    result+=new_letter
-  print(f"The decrypted text is {result}")
-
-
-if user_choice == "encode":
-  encode(text=user_text, shift=user_shift)
-if user_choice == "decode":
-  decode(text=user_text, shift=user_shift)
-  
+caesar(text=user_text, shift=user_shift)
